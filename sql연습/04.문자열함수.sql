21cb7a7e533f4f9a3262c11ebcd8f7763f1f3637
dana-developer <hdy78901@naver.com>
2024-12-10 15:55:41 +0900

    sql 연습 #2

---
+++ sql연습/04.문자열함수.sql
@@ -0,0 +1,33 @@
+--
+-- 문자열 함수
+-- 
+
+-- upper
+select upper('seoul'), ucase('SeouL') from dual;
+select upper(first_name) from employees;
+
+-- lower
+select lower('SEOUL'), lcase('SeouL') from dual;
+select lower(first_name) from employees;
+
+-- substring(문자열, index, length)
+select substring('hello world', 3, 2) from dual;
+
+-- 예제. employees 테이블에서 1989년에 입사한 직원의 이름, 입사일 출력
+select first_name, hire_date
+  from employees
+ where substring(hire_date, 1, 4) = '1989';
+ 
+-- lpad, rpad
+select lpad('1234',10, '-'), rpad('1234',10, '-') from dual;
+
+-- trim, ltrim, rtrim
+select 
+	concat('---', ltrim('    hello    '), '----'),
+	concat('---', rtrim('    hello    '), '----'),
+	concat('---', trim(leading 'x' from 'xxxhelloxxxx'), '----'),
+	concat('---', trim(trailing 'x' from 'xxxhelloxxxx'), '----'),
+	concat('---', trim(both 'x' from 'xxxhelloxxxx'), '----')
+from dual;
+
+select length("Hello World") from dual;
